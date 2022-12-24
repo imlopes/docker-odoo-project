a0150f8a93048df247f5db9eb1e7f08b53db2df4
ilo <dev@nomadiplus.com>
2022-12-24 18:58:39 -0300

    updating Docker file

--- 16.0-arm/install/dockerize.sh
+++ 16.0-arm/install/dockerize.sh
@@ -9,15 +9,15 @@ echo "DOCKERIZE Running on $arch"
 if [[ arch -eq 'aarch64' ]] || [[ arch -eq 'arm64' ]]; then
   echo "DOCKERIZE Running on arm64"
   arch="arm64"
-  hash="541ee4713933e087d766e2954b37cc652dff73b569d26b0c589277dcf8b16a9a"
-  curl -o dockerize-linux-${arch} https://github.com/powerman/dockerize/releases/download/v0.16.0/dockerize-linux-${arch} -SL
-  echo "${hash}  dockerize-linux-${arch}" | sha256sum -c -
-  install dockerize-linux-${arch} /usr/local/bin/dockerize && rm dockerize-linux-${arch}
+  hash="41adf4cacf82e952a771201c185e3b83ef76f240ea891e4c4f7a4ef6d39553fa"
 else
-  curl -o dockerize-linux-amd64-v0.6.0.tar.gz https://github.com/jwilder/dockerize/releases/download/v0.6.0/dockerize-linux-amd64-v0.6.0.tar.gz -SL
-  echo 'a13ff2aa6937f45ccde1f29b1574744930f5c9a5 dockerize-linux-amd64-v0.6.0.tar.gz' | sha1sum -c -
-  tar xvfz dockerize-linux-amd64-v0.6.0.tar.gz -C /usr/local/bin && rm dockerize-linux-amd64-v0.6.0.tar.gz
-  chmod +x /usr/local/bin/dockerize
+  echo "DOCKERIZE Running on x86_64"
+  arch="x86_64"
+  hash="9baf0af30e0ec0b47a0a34e0a2b2a0e5de1872d2f9e846829692b3457407cb29"
 fi
+
+curl -o dockerize-linux-${arch} https://github.com/powerman/dockerize/releases/download/v0.17.0/dockerize-linux-${arch} -SL
+echo "${hash}  dockerize-linux-${arch}" | sha256sum -c -
+install dockerize-linux-${arch} /usr/local/bin/dockerize && rm dockerize-linux-${arch}
 
 # TODO update version and repo
