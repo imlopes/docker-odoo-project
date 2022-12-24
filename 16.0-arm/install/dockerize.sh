cd164d7436f432c6fe906c4e35071084f19e12a6
ilo <dev@nomadiplus.com>
2022-12-24 13:46:41 -0300

    updating Docker file

--- 16.0-arm/install/dockerize.sh
+++ 16.0-arm/install/dockerize.sh
@@ -4,10 +4,12 @@ set -eo pipefail
 arch=`uname -m`
 echo "DOCKERIZE Running on $arch"
 if [[ arch -eq 'aarch64' ]]; then
+  echo "DOCKERIZE Running on arm64"
   arch="arm64"
   hash="541ee4713933e087d766e2954b37cc652dff73b569d26b0c589277dcf8b16a9a"
 else
   # assume x86_64
+  echo "DOCKERIZE Running on x86_64"
   hash="6981643dfb8e0b731f4b48b35d1fdd742b374ec66d1471a9ed7ed91781f2aca7"
 fi
 curl -o dockerize-linux-${arch} https://github.com/powerman/dockerize/releases/download/v0.16.0/dockerize-linux-${arch} -SL
