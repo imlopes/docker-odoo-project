895f7cc7f4f436a618c6d099b98f55b9a473ca93
ilo <dev@nomadiplus.com>
2023-02-04 16:09:23 -0300

    Working on 16.0-1.0.3

--- 16.0-arm/install/dockerize.sh
+++ 16.0-arm/install/dockerize.sh
@@ -5,16 +5,16 @@ arch=`uname -m`
 
 apt-get install -y build-essential gcc
 
-echo "DOCKERIZE Running on $arch"
-if [[ arch -eq 'aarch64' ]] || [[ arch -eq 'arm64' ]]; then
-  echo "DOCKERIZE Running on arm64"
-  arch="arm64"
-  hash="41adf4cacf82e952a771201c185e3b83ef76f240ea891e4c4f7a4ef6d39553fa"
-else
-  echo "DOCKERIZE Running on x86_64"
-  arch="x86_64"
-  hash="9baf0af30e0ec0b47a0a34e0a2b2a0e5de1872d2f9e846829692b3457407cb29"
-fi
+#echo "DOCKERIZE Running on $arch"
+#if [[ arch -eq 'aarch64' ]] || [[ arch -eq 'arm64' ]]; then
+#  echo "DOCKERIZE Running on arm64"
+#  arch="arm64"
+#  hash="41adf4cacf82e952a771201c185e3b83ef76f240ea891e4c4f7a4ef6d39553fa"
+#else
+echo "DOCKERIZE Running on x86_64"
+arch="x86_64"
+hash="9baf0af30e0ec0b47a0a34e0a2b2a0e5de1872d2f9e846829692b3457407cb29"
+#fi
 
 curl -o dockerize-linux-${arch} https://github.com/powerman/dockerize/releases/download/v0.17.0/dockerize-linux-${arch} -SL
 echo "${hash}  dockerize-linux-${arch}" | sha256sum -c -
